da6d87383a4692c8b6b3126bc4bdc8ba34e3592b
JamilsonMello <jamilsonmelow@gmail.com>
2020-08-31 20:34:35 -0300

    create user avatar controller

--- backend/src/modules/users/infra/http/routes/users.routes.ts
+++ backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -3,13 +3,13 @@ import multer from 'multer';
 
 import uploadConfig from '@config/upload';
 import UserController from '@modules/users/infra/http/controllers/UserController';
-import UpdateUserAvatarController from '@modules/users/infra/controllers/UpdateUserAvatarController';
+import UserAvatarController from '@modules/users/infra/http/controllers/UserAvatarController';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import * as validations from '../validations/user.routes';
 
 const usersRoutes = Router();
 const userController = new UserController();
-const updateUserAvatarController = new UpdateUserAvatarController();
+const userAvatarController = new UserAvatarController();
 
 const upload = multer(uploadConfig);
 
@@ -19,7 +19,7 @@ usersRoutes.patch(
   '/avatar',
   ensureAuthenticated,
   upload.single('avatar'),
-  updateUserAvatarController.update,
+  userAvatarController.update,
 );
 
 export default usersRoutes;
