71d7421b06c4aef3849aefff0fc89b79224d87b0
JamilsonMello <jamilsonmelow@gmail.com>
2020-08-31 20:21:53 -0300

    create recover password

---
+++ backend/src/modules/users/infra/http/controllers/ForgetPasswordController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import RecoverPasswordEmailService from '@modules/users/services/RecoverPasswordService';
+
+export default class ForgotPasswordController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { email } = request.body;
+
+    const recoverPasswordEmail = container.resolve(
+        RecoverPasswordEmailService,
+    );
+
+    await recoverPasswordEmail.execute({
+      email,
+    });
+
+    return response.status(204).json();
+  }
+}
