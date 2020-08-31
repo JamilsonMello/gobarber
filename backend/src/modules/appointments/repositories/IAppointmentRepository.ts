87608dce6c4f251dd6980c3c433778094510dfa6
JamilsonMello <jamilsonmelow@gmail.com>
2020-08-31 20:49:58 -0300

    create interface to Appointment

---
+++ backend/src/modules/appointments/repositories/IAppointmentRepository.ts
@@ -0,0 +1,15 @@
+import Appointment from '../infra/typeorm/entities/Appointment';
+import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
+import IFindAvailabilityInMonthDTO from '../dtos/IFindAvailabilityInMonthDTO';
+import IFindAvailabilityInDayDTO from '../dtos/IFindAvailabilityInDayDTO';
+
+export default interface IAppointmentsRepository {
+  create(data: ICreateAppointmentDTO): Promise<Appointment>;
+  findByDate(date: Date): Promise<Appointment | undefined>;
+  findAllInMonthFromProvider(
+    data: IFindAvailabilityInMonthDTO,
+  ): Promise<Appointment[]>;
+  findAllInDayFromProvider(
+    data: IFindAvailabilityInDayDTO,
+  ): Promise<Appointment[]>;
+}
