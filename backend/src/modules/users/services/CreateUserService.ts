613f549cfbfa73ded71415255e37582a0e33db28
JamilsonMello <jamilsonmelow@gmail.com>
2020-09-07 20:58:53 -0300

    clear cache with prefix name pass to invalidatePrefix

--- backend/src/modules/users/services/CreateUserService.ts
+++ backend/src/modules/users/services/CreateUserService.ts
@@ -3,6 +3,7 @@ import { injectable, inject } from 'tsyringe';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUserRepository from '@modules/users/repositories/IUserRepository';
 import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import AppError from '@shared/errors/AppError';
 
 interface IRequest {
@@ -17,6 +18,8 @@ class CreateUserSevices {
     @inject('UserRepository') private userRepository: IUserRepository,
 
     @inject('HashProvider') private hashProvider: IHashProvider,
+
+    @inject('CacheProvider') private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -33,6 +36,8 @@ class CreateUserSevices {
       email,
       password: password_hash,
     });
+
+    await this.cacheProvider.invalidatePrefix('providers-list');
 
     return user;
   }
